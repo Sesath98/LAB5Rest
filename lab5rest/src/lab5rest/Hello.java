a1add26d8c17a7c7d97b855a362352a7b9e69697
Sesath98 <bhanugaje@gmail.coom>
2021-03-19 22:15:29 +0530

    LAB5 REST

---
+++ lab5rest/src/lab5rest/Hello.java
@@ -0,0 +1,17 @@
+package lab5rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/Hello")
+public class Hello {
+
+	@GET
+	@Path("/{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloName(@PathParam("name") String name)
+	{
+	return "Hello " + name;
+	}
